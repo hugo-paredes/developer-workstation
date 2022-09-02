b62158fee4e32e316c1aeafe38fa1da9cdeb64cb
Hugo <hugo.nogueira.paredes@allianz.nl>
2022-09-02 14:29:32 +0200

    minor changes to script

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,24 +9,15 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-if [ $# -eq 0 ]
-  then
-    echo 'Enter a valid username.'
-    exit 1
-fi
+# Update system
+sudo apt update && sudo apt upgrade -y
+# Install dependencies
+sudo apt install -y python3.10 python3-pip git
+# Install ansible
+pip install ansible
+sudo apt install -y ansible-lint yamllint
 
-caller="$1"
-
-
-# # Update system
-# sudo apt update && sudo apt upgrade -y
-# # Install dependencies
-# sudo apt install -y python3.10 python3-pip git
-# # Install ansible
-# pip install ansible
-# sudo apt install -y ansible-lint yamllint
-
-cd "/home/$caller" && \
+cd "/home/$SUDO_USER" && \
   git clone https://github.com/hugo-paredes/developer-workstation.git && \
   cd developer-workstation && \
   ansible-playbook playbooks/main.yml
