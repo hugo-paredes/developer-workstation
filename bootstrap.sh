48eba644fabda9e73f842324eefdbb613c22c377
Hugo <hugo.nogueira.paredes@allianz.nl>
2022-09-02 11:19:01 +0200

    Added more to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,14 @@ fi
 # Update system
 sudo apt update && sudo apt upgrade -y
 # Install dependencies
-sudo apt install -y python3-pip git
+sudo apt install -y python3.10 python3-pip git
 # Install ansible
 pip install ansible
 sudo apt install -y ansible-lint yamllint
+
+cd ~ && \
+git clone https://github.com/hugo-paredes/developer-workstation.git && \
+cd developer-workstation && \
+ansible-playbook playbooks/main.yml
+
+exit 0
